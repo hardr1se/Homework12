07dfa6e208e7fae509582e450c414a187b15df1b
Stepan <stepan-morozov-1980@mail.ru>
2023-02-02 00:54:01 +0400

    Сделал двумя способами, подскажите пожалуйста какой лучше, мб какой-то неправильный вообще?

--- src/Book.java
+++ src/Book.java
@@ -9,6 +9,23 @@ public class Book {
         }
         public String getLastNameOfAuthor() { return this.lastNameOfAuthor; }
         public String getFirstNameOfAuthor() { return this.firstNameOfAuthor; }
+        @Override
+        public boolean equals(Object obj) {
+            if (this.getClass() != obj.getClass()) {
+                return false;
+            }
+            Author second = (Author) obj;
+            return getFirstNameOfAuthor().equals(second);
+        }
+
+        @Override
+        public int hashCode() {
+            return java.util.Objects.hash(lastNameOfAuthor, firstNameOfAuthor);
+        }
+        @Override
+        public String toString() {
+            return getLastNameOfAuthor() + " " + getFirstNameOfAuthor();
+        }
     }
     private int yearOfPublication;
     public Book(String nameOfBook, int yearOfPublication) {
@@ -18,6 +35,22 @@ public class Book {
     public String getName() { return this.nameOfBook; }
     public int getYearOfPublication() { return this.yearOfPublication; }
     public void setYearOfPublication(int yearOfPublication) { this.yearOfPublication = yearOfPublication; }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) { return true; }
+        if (!(obj instanceof Book)) return false;
+
+        Book book = (Book) obj;
+
+        if (yearOfPublication != book.yearOfPublication) return false;
+        return nameOfBook != null ? nameOfBook.equals(book.nameOfBook) : book.nameOfBook != null;
+    }
+    @Override
+    public int hashCode() {
+        int results = yearOfPublication;
+        results = 31 * results + (nameOfBook != null ? nameOfBook.hashCode() : 0);
+        return results;
+    }
     @Override
     public String toString() {
         return "Книга - " + getName() + " была опубликована в " + getYearOfPublication() +
